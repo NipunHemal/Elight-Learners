343e735a5d4dbb64a5882502d8cb47dca8d5ff8f
nipun-hub <nipunthemal@gmail.com>
2025-09-23 17:20:23 +0530

    main file stage link reffrance util

--- src/main/java/lk/ijse/elight_driving_school/Main.java
+++ src/main/java/lk/ijse/elight_driving_school/Main.java
@@ -5,12 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lk.ijse.elight_driving_school.util.ReferenceUtils;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-//        ReferenceUtils.primaryStage = stage;
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/Auth/Login.fxml"));
+        ReferenceUtils.primaryStage = stage;
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/layout/DashboardLayout.fxml"));
         Scene scene = new Scene(parent);
         stage.setScene(scene);
         stage.setTitle("Elight Driving School");
