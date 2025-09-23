4af724c9f850478aa3f028528b9967ca2461765b
nipun-hub <nipunthemal@gmail.com>
2025-09-23 12:47:30 +0530

    update main file

--- src/main/java/lk/ijse/elight_driving_school/Main.java
+++ src/main/java/lk/ijse/elight_driving_school/Main.java
@@ -1,4 +1,19 @@
 package lk.ijse.elight_driving_school;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+//        ReferenceUtils.primaryStage = stage;
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/Auth/Login.fxml"));
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.setTitle("Elight Driving School");
+        stage.show();
+    }
 }
