c3b50b0638db9203522b3dfdf02d48af033a97d2
nipun-hub <nipunthemal@gmail.com>
2025-09-23 17:24:37 +0530

    add login controller stack pane and it link refarance util

--- src/main/java/lk/ijse/elight_driving_school/controller/auth/LoginController.java
+++ src/main/java/lk/ijse/elight_driving_school/controller/auth/LoginController.java
@@ -1,13 +1,25 @@
 package lk.ijse.elight_driving_school.controller.auth;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import lk.ijse.elight_driving_school.util.ReferenceUtils;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public TextField usernameField;
     public PasswordField passwordField;
+    public StackPane dialogPane;
 
     public void onLogin(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ReferenceUtils.dialogPane = dialogPane;
     }
 }
