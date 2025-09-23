d22755d2a168365f6fd8dc64e02a31dfbd2bb338
nipun-hub <nipunthemal@gmail.com>
2025-09-23 12:45:41 +0530

    implement login page controller

---
+++ src/main/java/lk/ijse/elight_driving_school/controller/auth/LoginController.java
@@ -0,0 +1,13 @@
+package lk.ijse.elight_driving_school.controller.auth;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    public TextField usernameField;
+    public PasswordField passwordField;
+
+    public void onLogin(ActionEvent actionEvent) {
+    }
+}
