3f1446e57ceceeeb39e75c57e0e68594be56a19a
nipun-hub <nipunthemal@gmail.com>
2025-09-23 17:19:40 +0530

    update module-info.java info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,18 @@
 module lk.ijse.elight_driving_school {
     requires javafx.controls;
+    requires org.controlsfx.controls;
     requires javafx.fxml;
-
     requires org.kordamp.bootstrapfx.core;
+    requires static lombok;
+    requires com.jfoenix;
+    requires jdk.compiler;
+    requires jakarta.mail;
 
     opens lk.ijse.elight_driving_school to javafx.fxml;
+    opens lk.ijse.elight_driving_school.controller to javafx.fxml;
+    opens lk.ijse.elight_driving_school.controller.auth to javafx.fxml;
+    opens lk.ijse.elight_driving_school.controller.layout to javafx.fxml;
+//    opens lk.ijse.elight_driving_school.controller.component to javafx.fxml;
+//    opens lk.ijse.elight_driving_school.controller.component.user to javafx.fxml;
     exports lk.ijse.elight_driving_school;
 }
