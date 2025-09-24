4aa34c5554ac762ec4d5a9a31f23547e86bae3f1
nipun-hub <nipunthemal@gmail.com>
2025-09-24 08:38:31 +0530

    create Payment entity

---
+++ src/main/java/lk/ijse/elight_driving_school/entity/Payment.java
@@ -0,0 +1,26 @@
+package lk.ijse.elight_driving_school.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment {
+    @Id
+    private String paymentId;
+
+    private Date paymentDate;
+    private double amount;
+    private String paymentMethod;
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    private Student student;
+}
