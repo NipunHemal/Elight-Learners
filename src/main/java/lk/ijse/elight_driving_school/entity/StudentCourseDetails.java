6ba5da0ad5062a3b5f481ca3c747aa354b7f005f
nipun-hub <nipunthemal@gmail.com>
2025-09-24 08:42:23 +0530

    create StudentCourseDetails entity

---
+++ src/main/java/lk/ijse/elight_driving_school/entity/StudentCourseDetails.java
@@ -0,0 +1,29 @@
+package lk.ijse.elight_driving_school.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "student_course_details")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentCourseDetails {
+    @Id
+    private String studentCourseId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    private Date enrollmentDate;
+    private String status;
+    private String grade;
+}
