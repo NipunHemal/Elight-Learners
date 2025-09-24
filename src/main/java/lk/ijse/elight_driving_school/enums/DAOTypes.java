f531bf5bd28fd932e1aa6cc3b98f95442dcd1bdf
nipun-hub <nipunthemal@gmail.com>
2025-09-25 02:15:07 +0530

    Create DAOTypes AND ServiceFactory

---
+++ src/main/java/lk/ijse/elight_driving_school/enums/DAOTypes.java
@@ -0,0 +1,12 @@
+package lk.ijse.elight_driving_school.enums;
+
+public enum DAOTypes {
+    COURSE,
+    INSTRUCTORS,
+    LESSONS,
+    PAYMENTS,
+    STUDENT_COURSE_DETAILS,
+    STUDENTS,
+    USER,
+    QUERY
+}
---
+++ src/main/java/lk/ijse/elight_driving_school/enums/ServiceTypes.java
@@ -0,0 +1,12 @@
+package lk.ijse.elight_driving_school.enums;
+
+public enum ServiceTypes {
+    COURSE,
+    INSTRUCTOR,
+    LESSONS,
+    PAYMENTS,
+    QUERY,
+    STUDENT_COURSE_DETAILS,
+    STUDENT,
+    USER
+}
