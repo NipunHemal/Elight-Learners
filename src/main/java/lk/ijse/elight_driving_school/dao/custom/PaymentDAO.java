c892287e1a031c6fda010cec1f4fb3005c813cb4
nipun-hub <nipunthemal@gmail.com>
2025-09-25 02:13:40 +0530

    Create Payment

---
+++ src/main/java/lk/ijse/elight_driving_school/dao/custom/PaymentDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.elight_driving_school.dao.custom;
+
+import lk.ijse.elight_driving_school.dao.CrudDAO;
+import lk.ijse.elight_driving_school.entity.Payment;
+
+public interface PaymentDAO extends CrudDAO<Payment> {
+}
---
+++ src/main/java/lk/ijse/elight_driving_school/dao/custom/impl/PaymentsDAOImpl.java
@@ -0,0 +1,138 @@
+package lk.ijse.elight_driving_school.dao.custom.impl;
+
+import lk.ijse.elight_driving_school.config.FactoryConfig;
+import lk.ijse.elight_driving_school.dao.custom.PaymentDAO;
+import lk.ijse.elight_driving_school.entity.Payment;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PaymentsDAOImpl implements PaymentDAO {
+
+    private final FactoryConfig factoryConfig = FactoryConfig.getInstance();
+
+    @Override
+    public boolean save(Payment payments) throws Exception {
+        Session session = factoryConfig.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.persist(payments);
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            transaction.rollback();
+            return false;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean update(Payment payments) throws Exception {
+        Session session = factoryConfig.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.merge(payments);
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            transaction.rollback();
+            return false;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        Session session = factoryConfig.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Payment payments = (Payment) session.get(Payment.class, id);
+            if (payments != null) {
+                session.remove(payments);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<Payment> getAll() throws Exception {
+        Session session = factoryConfig.getSession();
+        try {
+            Query<Payment> query = session.createQuery("from Payment ",Payment.class);
+            List<Payment> paymentsList = query.list();
+            return paymentsList;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public String getLastId() throws Exception {
+        Session session = factoryConfig.getSession();
+        try {
+            Query<String> query = session.createQuery("SELECT p.paymentId FROM Payment p ORDER BY p.paymentId DESC", String.class)
+                    .setMaxResults(1);
+            List<String> paymentList = query.list();
+            if (paymentList.isEmpty()) {
+                return null;
+            }
+            return paymentList.getFirst();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<String> getAllIds() throws Exception {
+        Session session = factoryConfig.getSession();
+        try {
+            Query<String> query = session.createQuery("SELECT p.paymentId FROM  Payment p", String.class);
+            return query.list();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Optional<Payment> findById(String id) throws Exception {
+        Session session = factoryConfig.getSession();
+        try {
+            Payment payments = session.get(Payment.class, id);
+            return Optional.ofNullable(payments);
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public String generateNewId() {
+        String lastId = null;
+        try {
+            lastId = getLastId();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        if (lastId == null) {
+            return "P-001";
+        } else {
+            int num = Integer.parseInt(lastId.split("-")[1]);
+            num++;
+            return String.format("P-%03d", num);
+        }
+    }
+}
