eca9f10fc327a9fab098b1da9425cf3cd9395acb
nipun-hub <nipunthemal@gmail.com>
2025-09-24 05:38:22 +0530

    main add to paretn stack pain and link it reffarance util

--- src/main/java/lk/ijse/elight_driving_school/Main.java
+++ src/main/java/lk/ijse/elight_driving_school/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import lk.ijse.elight_driving_school.util.ReferenceUtils;
 
@@ -11,10 +12,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         ReferenceUtils.primaryStage = stage;
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/layout/DashboardLayout.fxml"));
-        Scene scene = new Scene(parent);
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/auth/Login.fxml"));
+        StackPane mainStackPane = new StackPane();
+        mainStackPane.getChildren().add(parent);
+        Scene scene = new Scene(mainStackPane);
         stage.setScene(scene);
         stage.setTitle("Elight Driving School");
         stage.show();
+        ReferenceUtils.mainStackPane = mainStackPane;
     }
 }
