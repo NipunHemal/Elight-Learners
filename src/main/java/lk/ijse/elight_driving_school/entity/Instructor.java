d9d7aa7ab73a51f3fe76b11e8e92f3da5974cd4f
nipun-hub <nipunthemal@gmail.com>
2025-09-24 08:38:21 +0530

    create Instructor entity

---
+++ src/main/java/lk/ijse/elight_driving_school/entity/Instructor.java
@@ -0,0 +1,34 @@
+package lk.ijse.elight_driving_school.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "instructors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Instructor {
+    @Id
+    private String instructorId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String contact;
+    private String specialization;
+    private String availability;
+
+    // One instructor can teach many courses
+    @OneToMany(mappedBy = "instructor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    private List<Course> courses = new ArrayList<>();
+
+    // One instructor can have many lessons
+    @OneToMany(mappedBy = "instructor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    private List<Lesson> lessons = new ArrayList<>();
+}
