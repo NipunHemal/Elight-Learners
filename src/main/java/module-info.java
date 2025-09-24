0272595d4d21c5ab1e7d421e4364447a8f4e6e47
nipun-hub <nipunthemal@gmail.com>
2025-09-24 05:37:54 +0530

    opens BreadcrumbController.pages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,6 +13,6 @@ module lk.ijse.elight_driving_school {
     opens lk.ijse.elight_driving_school.controller.auth to javafx.fxml;
     opens lk.ijse.elight_driving_school.controller.layout to javafx.fxml;
     opens lk.ijse.elight_driving_school.controller.component to javafx.fxml;
-//    opens lk.ijse.elight_driving_school.controller.component.user to javafx.fxml;
+    opens lk.ijse.elight_driving_school.controller.pages to javafx.fxml;
     exports lk.ijse.elight_driving_school;
 }
