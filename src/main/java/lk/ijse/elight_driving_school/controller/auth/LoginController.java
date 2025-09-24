21aeeb2a857ffe72df45e52f1c9363dcc3d049eb
nipun-hub <nipunthemal@gmail.com>
2025-09-24 05:37:20 +0530

    update login  ,  make click login button navigate dashboard

--- src/main/java/lk/ijse/elight_driving_school/controller/auth/LoginController.java
+++ src/main/java/lk/ijse/elight_driving_school/controller/auth/LoginController.java
@@ -5,8 +5,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import lk.ijse.elight_driving_school.util.AlertUtils;
+import lk.ijse.elight_driving_school.util.DashboardNavigator;
+import lk.ijse.elight_driving_school.util.DialogUtil;
 import lk.ijse.elight_driving_school.util.ReferenceUtils;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,11 +19,12 @@ public class LoginController implements Initializable {
     public PasswordField passwordField;
     public StackPane dialogPane;
 
-    public void onLogin(ActionEvent actionEvent) {
+    public void onLogin(ActionEvent actionEvent) throws IOException {
+        DashboardNavigator.mainNavigator("/view/layout/DashboardLayout.fxml");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ReferenceUtils.dialogPane = dialogPane;
+//        ReferenceUtils.mainStackPane = dialogPane;
     }
 }
