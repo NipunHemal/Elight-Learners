c37f950a04ae832862d659f7098104ea6046d1a6
nipun-hub <nipunthemal@gmail.com>
2025-09-24 07:45:22 +0530

    create UserDTO

--- src/main/java/lk/ijse/elight_driving_school/dto/UserDTO.java
+++ src/main/java/lk/ijse/elight_driving_school/dto/UserDTO.java
@@ -1,4 +1,19 @@
 package lk.ijse.elight_driving_school.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDTO {
+    private String userId;
+    private String username;
+    private String password;
+    private Role role;
+    private String email;
+    private String status;
 }
