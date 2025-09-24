892f20664207b5a902d7d2f8f635418c662a44f2
nipun-hub <nipunthemal@gmail.com>
2025-09-25 02:08:57 +0530

    Create CourseD

---
+++ src/main/java/lk/ijse/elight_driving_school/dao/custom/CourseDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.elight_driving_school.dao.custom;
+
+import lk.ijse.elight_driving_school.dao.CrudDAO;
+import lk.ijse.elight_driving_school.entity.Course;
+
+public interface CourseDAO extends CrudDAO<Course> {
+}
