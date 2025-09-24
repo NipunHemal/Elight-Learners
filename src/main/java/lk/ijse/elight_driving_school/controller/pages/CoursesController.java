e692c6802cf5ab052d0664a6fa5cb59890f825f2
nipun-hub <nipunthemal@gmail.com>
2025-09-25 03:14:45 +0530

    init ui variyable and methods

--- src/main/java/lk/ijse/elight_driving_school/controller/pages/CoursesController.java
+++ src/main/java/lk/ijse/elight_driving_school/controller/pages/CoursesController.java
@@ -1,4 +1,33 @@
 package lk.ijse.elight_driving_school.controller.pages;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CoursesController {
+
+    @FXML
+    private TableColumn<?, ?> colAccount;
+
+    @FXML
+    private TableColumn<?, ?> colAction;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colProjects;
+
+    @FXML
+    private TableColumn<?, ?> cplAccessExpire;
+
+    @FXML
+    private TableView<?> membersTable;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
 }
