baedcdf00fb987e37a2a3544624f66b2bc8199ef
nipun-hub <nipunthemal@gmail.com>
2025-09-25 02:08:07 +0530

    implement UserService

--- src/main/java/lk/ijse/elight_driving_school/service/custom/UserService.java
+++ src/main/java/lk/ijse/elight_driving_school/service/custom/UserService.java
@@ -1,4 +1,27 @@
 package lk.ijse.elight_driving_school.service.custom;
 
-public interface UserService {
+import lk.ijse.elight_driving_school.dto.UserDTO;
+import lk.ijse.elight_driving_school.service.SuperService;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService extends SuperService {
+    List<UserDTO> getAllUsers() throws Exception;
+
+    String getLastUserId() throws Exception;
+
+    boolean saveUsers(UserDTO t) throws Exception;
+
+    boolean updateUsers(UserDTO t) throws Exception;
+
+    boolean deleteUsers(String id) throws Exception;
+
+    List<String> getAllUserIds() throws Exception;
+
+    Optional<UserDTO> findByUserId(String id) throws Exception;
+
+    String generateNextUserId();
+
+    public UserDTO getUserByEmail(String email) ;
 }
--- src/main/java/lk/ijse/elight_driving_school/service/custom/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/elight_driving_school/service/custom/impl/UserServiceImpl.java
@@ -1,4 +1,86 @@
 package lk.ijse.elight_driving_school.service.custom.impl;
 
-public class UserServiceImpl {
+import lk.ijse.elight_driving_school.enums.DAOTypes;
+import lk.ijse.elight_driving_school.mapper.UserMapper;
+import lk.ijse.elight_driving_school.service.custom.UserService;
+import lk.ijse.elight_driving_school.service.exception.DuplicateException;
+import lk.ijse.elight_driving_school.service.exception.NotFoundException;
+import lk.ijse.elight_driving_school.dao.DAOFactory;
+import lk.ijse.elight_driving_school.dao.custom.UserDAO;
+import lk.ijse.elight_driving_school.dto.UserDTO;
+import lk.ijse.elight_driving_school.entity.User;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    private  final UserDAO userDAO = DAOFactory.getInstance().getDAO(DAOTypes.USER);
+
+    @Override
+    public List<UserDTO> getAllUsers() throws Exception {
+        List<User> userList = userDAO.getAll();
+        List<UserDTO> userDTOList = new ArrayList<>();
+        for (User user : userList) {
+            userDTOList.add(UserMapper.toDTO(user));
+        }
+        return userDTOList;
+    }
+
+    @Override
+    public String getLastUserId() throws Exception {
+        return userDAO.getLastId();
+    }
+
+    @Override
+    public boolean saveUsers(UserDTO t) throws Exception {
+        Optional<User> user = userDAO.findById(t.getUserId());
+        if (user.isPresent()) {
+            throw new DuplicateException("User already exists");
+        }
+        return userDAO.save(UserMapper.toEntity(t));
+
+    }
+
+    @Override
+    public boolean updateUsers(UserDTO t) throws Exception {
+        Optional<User> user = userDAO.findById(t.getUserId());
+        if (user.isEmpty()) {
+            throw new NotFoundException("User not found");
+        }
+        return userDAO.update(UserMapper.toEntity(t));
+    }
+
+    @Override
+    public boolean deleteUsers(String id) throws Exception {
+        Optional<User> user = userDAO.findById(id);
+        if (user.isEmpty()) {
+            throw new NotFoundException("User not found");
+        }
+        return userDAO.delete(id);
+    }
+
+    @Override
+    public List<String> getAllUserIds() throws Exception {
+        return userDAO.getAllIds();
+    }
+
+    @Override
+    public Optional<UserDTO> findByUserId(String id) throws Exception {
+        Optional<User> user = userDAO.findById(id);
+        return user.map(UserMapper::toDTO);
+    }
+
+    @Override
+    public String generateNextUserId() {
+        return userDAO.generateNewId();
+    }
+
+    @Override
+    public UserDTO getUserByEmail(String email) {
+        User user = userDAO.getUserByEmail(email);
+        return UserMapper.toDTO(user);
+    }
 }
