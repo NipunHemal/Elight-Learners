63b8a55bf5f4bb9fa783ff4d220d2ed693d2228b
nipun-hub <nipunthemal@gmail.com>
2025-09-25 02:13:31 +0530

    Create LessonsDAO

---
+++ src/main/java/lk/ijse/elight_driving_school/dao/custom/impl/LessonsDAOImpl.java
@@ -0,0 +1,141 @@
+package lk.ijse.elight_driving_school.dao.custom.impl;
+
+import lk.ijse.elight_driving_school.config.FactoryConfig;
+import lk.ijse.elight_driving_school.dao.custom.LessonsDAO;
+import lk.ijse.elight_driving_school.entity.Lesson;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LessonsDAOImpl implements LessonsDAO {
+    private final FactoryConfig factoryConfig = FactoryConfig.getInstance();
+
+
+    @Override
+    public boolean save(Lesson lessons) throws Exception {
+        Session session = factoryConfig.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.persist(lessons);
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            transaction.rollback();
+            return false;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean update(Lesson lessons) throws Exception {
+        Session session = factoryConfig.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.merge(lessons);
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            transaction.rollback();
+            return false;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        Session session = factoryConfig.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Lesson lessons = (Lesson) session.get(Lesson.class, id);
+            if (lessons != null) {
+                session.remove(lessons);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+
+    @Override
+    public List<Lesson> getAll() throws Exception {
+        Session session = factoryConfig.getSession();
+        try {
+            Query<Lesson> query = session.createQuery("from Lesson ",Lesson.class);
+            List<Lesson> lessonsList = query.list();
+            return lessonsList;
+        }finally {
+            session.close();
+        }
+
+    }
+
+    @Override
+    public String getLastId() throws Exception {
+        Session session = factoryConfig.getSession();
+        try {
+            Query<String> query = session.createQuery("SELECT l.lessonId FROM Lesson l ORDER BY l.lessonId DESC", String.class)
+                    .setMaxResults(1);
+            List<String> lessonsList = query.list();
+            if (lessonsList.isEmpty()) {
+                return null;
+            }
+            return lessonsList.getFirst();
+        } finally {
+            session.close();
+        }
+    }
+
+
+    @Override
+    public List<String> getAllIds() throws Exception {
+        Session session = factoryConfig.getSession();
+        try {
+            Query<String> query = session.createQuery("SELECT l.lessonId FROM Lesson l", String.class);
+            return query.list();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Optional<Lesson> findById(String id) throws Exception {
+        Session session = factoryConfig.getSession();
+        try {
+            Lesson lessons = session.get(Lesson.class, id);
+            return Optional.ofNullable(lessons);
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public String generateNewId() {
+        String lastId = null;
+        try {
+            lastId = getLastId();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        if (lastId == null) {
+            return "L-001";
+        } else {
+            int num = Integer.parseInt(lastId.split("-")[1]);
+            num++;
+            return String.format("L-%03d", num);
+        }
+    }
+}
