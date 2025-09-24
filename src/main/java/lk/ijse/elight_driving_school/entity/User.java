44c32118c101eaf832dab892f99a7dec61116b79
nipun-hub <nipunthemal@gmail.com>
2025-09-24 08:42:30 +0530

    create User entity

---
+++ src/main/java/lk/ijse/elight_driving_school/entity/User.java
@@ -0,0 +1,28 @@
+package lk.ijse.elight_driving_school.entity;
+
+import jakarta.persistence.*;
+import lk.ijse.elight_driving_school.enums.Role;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private String userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String email;
+    private String status;
+}
