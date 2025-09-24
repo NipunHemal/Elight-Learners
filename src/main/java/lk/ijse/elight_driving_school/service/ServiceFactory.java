2a7b1c73178167d913100941c3a61ad48bc3f517
nipun-hub <nipunthemal@gmail.com>
2025-09-25 02:16:15 +0530

    Create serviceFactory

--- src/main/java/lk/ijse/elight_driving_school/service/ServiceFactory.java
+++ src/main/java/lk/ijse/elight_driving_school/service/ServiceFactory.java
@@ -1,4 +1,19 @@
 package lk.ijse.elight_driving_school.service;
 
+import lk.ijse.elight_driving_school.dao.custom.impl.*;
+import lk.ijse.elight_driving_school.enums.ServiceTypes;
+
 public class ServiceFactory {
+    private static ServiceFactory serviceFactory;
+    private ServiceFactory() {}
+
+    public static ServiceFactory getInstance() {
+        return serviceFactory == null ? (serviceFactory = new ServiceFactory()) : serviceFactory;
+    }
+
+    public <T extends SuperService> T getService(ServiceTypes serviceTypes) {
+        return switch (serviceTypes) {
+            case COURSE ->  (T) new CourServiceImpl();
+        };
+    }
 }
