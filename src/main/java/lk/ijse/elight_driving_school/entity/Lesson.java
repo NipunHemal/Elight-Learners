4051982b5f21a8efb32b17f340f23f96adb955de
nipun-hub <nipunthemal@gmail.com>
2025-09-24 08:38:14 +0530

    create Lesson entity

---
+++ src/main/java/lk/ijse/elight_driving_school/entity/Lesson.java
@@ -0,0 +1,35 @@
+package lk.ijse.elight_driving_school.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Entity
+@Table(name = "lessons")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Lesson {
+    @Id
+    private String lessonId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    private Date lessonDate;
+    private Time startTime;
+    private Time endTime;
+    private String status;
+}
