d72faf750fe65578774bbbaa401aece59f2f65ee
nipun-hub <nipunthemal@gmail.com>
2025-09-24 17:29:06 +0530

    implement CourseBO and CourseBOImpl.java

---
+++ src/main/java/lk/ijse/elight_driving_school/service/custom/CourseBO.java
@@ -0,0 +1,26 @@
+package lk.ijse.elight_driving_school.service.custom;
+
+import lk.ijse.elight_driving_school.service.SuperService;
+import lk.ijse.elight_driving_school.dto.CourseDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CourseBO extends SuperService {
+    List<CourseDTO> getAllCourses() throws Exception;
+
+    String getLastCourseId() throws Exception;
+
+    boolean saveCourses(CourseDTO t) throws Exception;
+
+    boolean updateCourses(CourseDTO t) throws Exception;
+
+    boolean deleteCourses(String id) throws Exception;
+
+    List<String> getAllCourseIds() throws Exception;
+
+    Optional<CourseDTO> findByCourseId(String id) throws Exception;
+
+    String generateNewCourseId();
+
+}
