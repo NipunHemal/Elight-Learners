3f022c807b564769ce427edf77caf0804bae21db
nipun-hub <nipunthemal@gmail.com>
2025-09-25 11:41:31 +0530

    create Patment tm

---
+++ src/main/java/lk/ijse/elight_driving_school/dto/tm/LessonsTM.java
@@ -0,0 +1,27 @@
+package lk.ijse.elight_driving_school.dto.tm;
+
+import javafx.scene.layout.Pane;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class LessonsTM {
+    private String lessonId;
+    private String studentId;
+    private String courseId;
+    private String instructorId;
+    private Date lessonDate;
+    private String startTime;
+    private String endTime;
+    private String status;
+    private Pane action;
+
+
+}
---
+++ src/main/java/lk/ijse/elight_driving_school/dto/tm/PaymentsTM.java
@@ -0,0 +1,22 @@
+package lk.ijse.elight_driving_school.dto.tm;
+
+import javafx.scene.layout.Pane;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class PaymentsTM {
+    private String paymentId;
+    private Date paymentDate;
+    private double amount;
+    private String paymentMethod;
+    private String status;
+    private String studentId;
+    private Pane action;
+}
