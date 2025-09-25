21707c6f9997dfdef1b6446a73527df0c2854b99
nipun-hub <nipunthemal@gmail.com>
2025-09-25 21:51:30 +0530

    update StudentController

--- src/main/java/lk/ijse/elight_driving_school/controller/pages/StudentController.java
+++ src/main/java/lk/ijse/elight_driving_school/controller/pages/StudentController.java
@@ -2,43 +2,53 @@ package lk.ijse.elight_driving_school.controller.pages;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.elight_driving_school.dto.tm.StudentTM;
+import lk.ijse.elight_driving_school.util.NotificationUtils;
 
-public class StudentController {
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
 
-    public TableView tableStudent;
+public class StudentController implements Initializable {
+
+    public TableView<StudentTM> tableStudent;
     @FXML
-    private TableColumn<?, ?> colAction;
-
-    @FXML
-    private TableColumn<?, ?> colAddress;
-
-    @FXML
-    private TableColumn<?, ?> colDob;
-
-    @FXML
-    private TableColumn<?, ?> colEmail;
+    private TableColumn<StudentTM , String> colAction;
 
     @FXML
-    private TableColumn<?, ?> colFirstname;
+    private TableColumn<StudentTM , String> colAddress;
 
     @FXML
-    private TableColumn<?, ?> colId;
+    private TableColumn<StudentTM , Date> colDob;
 
     @FXML
-    private TableColumn<?, ?> colLastname;
+    private TableColumn<StudentTM , String> colEmail;
 
     @FXML
-    private TableColumn<?, ?> colPhone;
+    private TableColumn<StudentTM , String> colFirstname;
 
     @FXML
-    private TableColumn<?, ?> colRegDate;
+    private TableColumn<StudentTM , String> colId;
 
     @FXML
-    private TableView<?> tableCourse;
+    private TableColumn<StudentTM , String> colLastname;
+
+    @FXML
+    private TableColumn<StudentTM , String> colPhone;
+
+    @FXML
+    private TableColumn<StudentTM , Date> colRegDate;
+
+    @FXML
+    private TableColumn<StudentTM , String> colCourseId;
+
+
 
     @FXML
     private TextField tstSearch;
@@ -50,5 +60,32 @@ public class StudentController {
     void btnAddOnAction(ActionEvent event) {
 
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try{
+            initializeTable();
+            loadStudent();
+        } catch (Exception e) {
+            NotificationUtils.showError("Error", e.getMessage());
+        }
+    }
+
+    private void loadStudent() {
+        colId.setCellValueFactory(new PropertyValueFactory<>("studentId"));
+        colFirstname.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        colLastname.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colDob.setCellValueFactory(new PropertyValueFactory<>("dob"));
+        colRegDate.setCellValueFactory(new PropertyValueFactory<>("registrationDate"));
+        colCourseId.setCellValueFactory(new PropertyValueFactory<>("courseId"));
+        colAction.setCellValueFactory(new PropertyValueFactory<>("action"));
+    }
+
+    private void initializeTable() {
+    }
+
 
 }
