6c7dc195494f915d23107b21d8c4e477fbc5e1f8
nipun-hub <nipunthemal@gmail.com>
2025-09-25 10:48:49 +0530

    update Instructor.fxml and InstructorController

--- src/main/java/lk/ijse/elight_driving_school/controller/pages/InstructorController.java
+++ src/main/java/lk/ijse/elight_driving_school/controller/pages/InstructorController.java
@@ -1,4 +1,51 @@
 package lk.ijse.elight_driving_school.controller.pages;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class InstructorController {
+
+    public TableView tableInstructor;
+    @FXML
+    private TableColumn<?, ?> colAvailability;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colFirstname;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colLastname;
+
+    @FXML
+    private TableColumn<?, ?> colSpecialization;
+
+    @FXML
+    private TableColumn<?, ?> cplContact;
+
+    @FXML
+    private TableColumn<?, ?> p;
+
+    @FXML
+    private TableView<?> tableCourse;
+
+    @FXML
+    private TextField tstSearch;
+
+    @FXML
+    private Label txtCourseCount;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
 }
