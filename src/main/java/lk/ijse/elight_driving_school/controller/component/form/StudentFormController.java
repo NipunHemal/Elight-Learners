425af72497fbb9f00610ea2864e92e19a5ff8eaf
nipun-hub <nipunthemal@gmail.com>
2025-09-25 08:39:00 +0530

    create StudentForm.fxml and StudentFormController

---
+++ src/main/java/lk/ijse/elight_driving_school/controller/component/form/StudentFormController.java
@@ -0,0 +1,53 @@
+package lk.ijse.elight_driving_school.controller.component.form;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class StudentFormController {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private DatePicker dobPicker;
+
+    @FXML
+    private Label emailError;
+
+    @FXML
+    private Label lblFormSubTitle;
+
+    @FXML
+    private Label lblFormTitle;
+
+    @FXML
+    private DatePicker registstaionDatePicker;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button saveButton1;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+}
