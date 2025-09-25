3045b7ad59845ed0f1ce83dcf5620e6d5413d436
nipun-hub <nipunthemal@gmail.com>
2025-09-25 21:51:47 +0530

    update UserController

--- src/main/java/lk/ijse/elight_driving_school/controller/pages/UserController.java
+++ src/main/java/lk/ijse/elight_driving_school/controller/pages/UserController.java
@@ -2,33 +2,40 @@ package lk.ijse.elight_driving_school.controller.pages;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import lk.ijse.elight_driving_school.dto.tm.UserTM;
+import lk.ijse.elight_driving_school.util.NotificationUtils;
 
-public class UserController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private TableColumn<?, ?> colAction;
-
-    @FXML
-    private TableColumn<?, ?> colEmail;
-
-    @FXML
-    private TableColumn<?, ?> colId;
+public class UserController implements Initializable {
 
     @FXML
-    private TableColumn<?, ?> colRole;
+    private TableColumn<UserTM, Pane> colAction;
 
     @FXML
-    private TableColumn<?, ?> colStatus;
+    private TableColumn<UserTM , String> colEmail;
 
     @FXML
-    private TableColumn<?, ?> colUsername;
+    private TableColumn<UserTM , String> colId;
 
     @FXML
-    private TableView<?> tableUser;
+    private TableColumn<UserTM , String> colRole;
+
+    @FXML
+    private TableColumn<UserTM , String> colStatus;
+
+    @FXML
+    private TableColumn<UserTM , String> colUsername;
+
+
 
     @FXML
     private TextField tstSearch;
@@ -39,6 +46,28 @@ public class UserController {
     @FXML
     void btnAddOnAction(ActionEvent event) {
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try{
+            initializeTable();
+            loadUser();
+        } catch (Exception e) {
+            NotificationUtils.showError("Error", e.getMessage());
+        }
+    }
+
+    private void initializeTable() {
+        colId.setCellValueFactory(new PropertyValueFactory<>("userId"));
+        colUsername.setCellValueFactory(new PropertyValueFactory<>("username"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colRole.setCellValueFactory(new PropertyValueFactory<>("role"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+        colAction.setCellValueFactory(new PropertyValueFactory<>("action"));
+
+    }
+    private void loadUser() {
     }
 
 }
