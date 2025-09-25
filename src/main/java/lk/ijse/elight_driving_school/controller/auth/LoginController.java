2878c641869bb4659b95597b004a0606ab2c08f2
nipun-hub <nipunthemal@gmail.com>
2025-09-25 09:11:47 +0530

    Compleate Login Flow

--- src/main/java/lk/ijse/elight_driving_school/controller/auth/LoginController.java
+++ src/main/java/lk/ijse/elight_driving_school/controller/auth/LoginController.java
@@ -1,30 +1,55 @@
 package lk.ijse.elight_driving_school.controller.auth;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
-import lk.ijse.elight_driving_school.util.AlertUtils;
-import lk.ijse.elight_driving_school.util.DashboardNavigator;
-import lk.ijse.elight_driving_school.util.DialogUtil;
-import lk.ijse.elight_driving_school.util.ReferenceUtils;
+import lk.ijse.elight_driving_school.dto.UserDTO;
+import lk.ijse.elight_driving_school.enums.ServiceTypes;
+import lk.ijse.elight_driving_school.service.ServiceFactory;
+import lk.ijse.elight_driving_school.service.custom.UserService;
+import lk.ijse.elight_driving_school.util.*;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class LoginController implements Initializable {
+public class LoginController{
     public TextField usernameField;
     public PasswordField passwordField;
     public StackPane dialogPane;
+    UserService userService = ServiceFactory.getInstance().getService(ServiceTypes.USER);
 
     public void onLogin(ActionEvent actionEvent) throws IOException {
-        DashboardNavigator.mainNavigator("/view/layout/DashboardLayout.fxml");
+        if (validate()){
+            String username = usernameField.getText();
+            String password = passwordField.getText();
+
+            UserDTO user = userService.getUserByEmail(username);
+
+            if (user == null){
+                NotificationUtils.showError("Login Error","User not found for "+username);
+                return;
+            }
+
+            if (user != null && PasswordUtils.checkPassword(password,user.getPassword())){
+                AlertUtils.showSuccess("Login Success","Login Successfully");
+                AuthUtil.setCurrentUser(user);
+                DashboardNavigator.mainNavigator("/view/layout/DashboardLayout.fxml");
+            } else {
+                AlertUtils.showError("Login Error","Invalid Username or Password");
+            }
+        }
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        ReferenceUtils.mainStackPane = dialogPane;
+    public boolean validate(){
+        if (usernameField.getText().isEmpty()){
+            NotificationUtils.showError("Validation Error","Username is required");
+            usernameField.requestFocus();
+            return false;
+        } else if (passwordField.getText().isEmpty()) {
+            NotificationUtils.showError("Validation Error","Password is required");
+            passwordField.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
